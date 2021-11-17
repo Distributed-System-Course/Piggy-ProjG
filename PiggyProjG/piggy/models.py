9c50a5aff719fdb0e0b489ce56692623e7e42fc2
henry_23 <henry.maoht@outlook.com>
2021-11-18 02:12:19 +0800

    feat(models): finish model design
    Co-authored-by: Par <liberty_chaser@outlook.com>
    Co-authored-by: nanjialin <nanjialin_2000@163.com>

--- PiggyProjG/piggy/models.py
+++ PiggyProjG/piggy/models.py
@@ -12,6 +12,7 @@ class Student(models.Model):
     username = models.CharField(max_length=150)
     password = models.CharField(max_length=128)
     name = models.CharField(max_length=150)
+    rank = models.IntegerField(default=0)
 
 
 class ProjectGroup(models.Model):
@@ -22,5 +23,19 @@ class ProjectGroup(models.Model):
 class Project(models.Model):
     project_group = models.ForeignKey(ProjectGroup, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
-    detail = models.CharField(max_length=500)
-    max_group = models.IntegerField(default=5)
+    description = models.CharField(max_length=500)
+    max_group_num = models.IntegerField(default=5)
+    max_team_member_num = models.IntegerField(default=5)
+
+
+class Team(models.Model):
+    project_group = models.ForeignKey(ProjectGroup, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    members = models.ManyToManyField(Student)
+
+
+class TeamWishes(models.Model):
+    group = models.ForeignKey(Team, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    priority = models.IntegerField()
