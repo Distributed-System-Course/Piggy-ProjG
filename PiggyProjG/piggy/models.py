8209f1123af900388ea0473c12137b9a0536b132
henry_23 <henry.maoht@outlook.com>
2021-11-18 06:50:48 +0800

    fix(django-models): `__str__`s & field rename

--- PiggyProjG/piggy/models.py
+++ PiggyProjG/piggy/models.py
@@ -8,7 +8,7 @@ class Teacher(models.Model):
     name = models.CharField(max_length=150)
 
     def __str__(self):
-        return "{}({})".format(self.name, self.username)
+        return "{}".format(self.name, self.username)
 
 
 class Student(models.Model):
@@ -18,7 +18,7 @@ class Student(models.Model):
     rank = models.IntegerField(default=0)
     
     def __str__(self):
-        return "{}({})".format(self.name, self.username)
+        return "{}".format(self.name, self.username)
 
 
 class ProjectGroup(models.Model):
@@ -26,7 +26,7 @@ class ProjectGroup(models.Model):
     name = models.CharField(max_length=150)
 
     def __str__(self):
-        return "{}({})".format(self.name, self.teacher)
+        return "{} ({})".format(self.name, self.teacher)
 
 
 class Project(models.Model):
@@ -37,7 +37,7 @@ class Project(models.Model):
     max_team_member_num = models.IntegerField(default=5)
     
     def __str__(self):
-        return "{}({})".format(self.name, self.project_group)
+        return "{} in {}".format(self.name, self.project_group)
 
 
 class Team(models.Model):
@@ -47,13 +47,13 @@ class Team(models.Model):
     members = models.ManyToManyField(Student)
 
     def __str__(self):
-        return "{}: {} ({})".format(self.name, self.project, self.project_group)
+        return "{} @ {}".format(self.name, self.project_group)
 
 
 class TeamWish(models.Model):
-    group = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     priority = models.IntegerField()
     
     def __str__(self):
-        return "{} #{} {} ({})".format(self.group, self.priority, self.project)
+        return "{} #{} {}".format(self.team.name, self.priority, self.project)
