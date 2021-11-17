b6f796d1662c53abbac4c87338d77e7b26440ef6
henry_23 <henry.maoht@outlook.com>
2021-11-18 02:47:22 +0800

    fix(piggy): re-generate migrations & add `__str__`

--- PiggyProjG/piggy/models.py
+++ PiggyProjG/piggy/models.py
@@ -7,17 +7,26 @@ class Teacher(models.Model):
     password = models.CharField(max_length=128)
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return "{}({})".format(self.name, self.username)
+
 
 class Student(models.Model):
     username = models.CharField(max_length=150)
     password = models.CharField(max_length=128)
     name = models.CharField(max_length=150)
     rank = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return "{}({})".format(self.name, self.username)
 
 
 class ProjectGroup(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return "{}({})".format(self.name, self.teacher)
 
 
 class Project(models.Model):
@@ -26,6 +35,9 @@ class Project(models.Model):
     description = models.CharField(max_length=500)
     max_group_num = models.IntegerField(default=5)
     max_team_member_num = models.IntegerField(default=5)
+    
+    def __str__(self):
+        return "{}({})".format(self.name, self.project_group)
 
 
 class Team(models.Model):
@@ -34,8 +46,14 @@ class Team(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     members = models.ManyToManyField(Student)
 
+    def __str__(self):
+        return "{}: {} ({})".format(self.name, self.project, self.project_group)
 
-class TeamWishes(models.Model):
+
+class TeamWish(models.Model):
     group = models.ForeignKey(Team, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     priority = models.IntegerField()
+    
+    def __str__(self):
+        return "{} #{} {} ({})".format(self.group, self.priority, self.project)
